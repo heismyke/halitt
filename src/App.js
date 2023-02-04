173bc07d0911da6fe5845e83bb5e0eef73fba7f8
myke <81974207+heismyke@users.noreply.github.com>
2023-02-04 05:24:31 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import TopNav from './Components/TopNav/Navbar'
-
+import './assets/css/style.css'
+import Navigation from '././Components/NavigationBar/Nav'
+import Herosection from '././Components/HeroSection/Hero.jsx'
 function App() {
   return (
     <div>
-       <TopNav />
+       <Navigation />
+       <Herosection />
     </div>
   );
 }
---
+++ src/Components/HeroSection/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './hero.css'
+import heromages from '../HeroSection/Heroimages/heroTwo.jpeg'
+const Hero = () => {
+  return (
+   <div className='hero'>
+        <div className='heroText'>
+
+        </div>
+        <div className='heroImage heroImage2'>
+
+        </div>
+   </div>
+  )
+}
+
+export default Hero
