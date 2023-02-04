37fd01e8f538d199c5188da340fcc5910c406a28
myke <81974207+heismyke@users.noreply.github.com>
2023-02-04 18:51:58 +0100

    first commit

--- src/Components/Subhead/Subheading.jsx
+++ src/Components/Subhead/Subheading.jsx
@@ -2,8 +2,35 @@ import React from 'react'
 
 const Subheading = () => {
   return (
-    <div>
-        
+    <div className='row bg-white d-flex justify-content-center flex-md-row flex-column'>
+        <div className='col-12 col-md-4 bg-none pt-5 text-center text-md-start'>
+          <h1>About the project</h1>
+        </div>
+        <div className='col-12 col-md-6 bg-none pt-5 text-center text-md-start'>
+           
+            <div className='col-md-12'>
+            <p className='p-md-0 p-2 fs-5 '>
+            The inspiration for the creation of the concept was the original architecture of the House itself: the geometric elements of the front facade, attracting attention, bright and spacious rooms with five-meter ceilings, immediately refer to the aesthetics of the American classical Art Deco. </p>
+
+            <p className='p-md-0 p-2 fs-5'> Its influence quickly swept America at the beginning of the 19th century, formed a way of life of a period characterized by rapid economic growth, the development of technology, mechanical engineering and construction of cult famous art structures, as well as the development of film art.</p>
+            </div>
+            <div className='col-md-12'>
+                <div className="row border-top border-dark">
+                    <div className="col-md-4">
+                        <h1>40</h1>
+                        <p>Apartment</p>
+                    </div>
+                    <div className="col-md-4">
+                    <h1>10</h1>
+                        <p>Penthouse</p>
+                    </div>
+                    <div className="col-md-4">
+                    <h1>10</h1>
+                        <p>Floor</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
